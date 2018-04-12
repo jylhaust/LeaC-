9448fa888303868a34dde79de6da246d9adc5f12
jylhaust <jylhaust@163.com>
2018-04-12 16:17:02 +0800

    update

--- base/readtxt/main.cpp
+++ base/readtxt/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <fstream>
 
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string fileroute;
+    fileroute = "/home/yuanlin/Desktop/test.txt";
+    /*
+    fstream fin(fileroute);
+    if (!fin)
+        cout<<"cannot find txt"<<endl;
+    while(!fin.eof())
+    {
+        string cpt;
+        fin>>cpt;
+        cout<<"print code :"<<cpt<<endl;
+    }*/
+    fstream fin;
+    string s;
+    fin.open("/home/yuanlin/Desktop/test.txt",ios::in); //ios::in|ios::out|ios::binary
+    while (getline(fin,s))
+    {
+        cout<<s<<endl;
+    }
+    fin.close();
     return 0;
 }
